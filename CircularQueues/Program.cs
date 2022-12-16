0402e92d1673f3792b3002597e6806ee0eeadd58
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2022-12-16 17:13:53 +0700

    membuat class Queues untuk nantinya sebagai antian yang akan dibuat

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace CircularQueues
 {
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+
+    }
     class Program
     {
         static void Main(string[] args)
